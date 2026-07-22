0907a8bd1289215efd68a44f7464f648badae675
Mohaned <mohandanw@gmail.com>
2026-07-22 06:36:15 +0300

    delete files

--- test_casual.py
+++
@@ -1,8 +0,0 @@
-from src.agents.state import AgentState, Message
-from src.agents.casual_chat import casual_chat_node
-import asyncio
-import os
-os.environ["TAVILY_API_KEY"] = "tvly-DUMMYKEY"
-state = AgentState(messages=[Message(role="user", content="Hello")], tasks=[], current_task_id=None, next_agent="Casual_Chat_Agent", query_intent="casual_chat", answer="", username="test")
-res = casual_chat_node(state)
-print(res)
--- test_langchain_tuples.py
+++
@@ -1,3 +0,0 @@
-from langchain_core.messages import convert_to_messages
-msgs = [("user", "hello"), ("assistant", "world")]
-print(convert_to_messages(msgs))
